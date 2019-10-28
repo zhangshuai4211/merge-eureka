256e887ec99a76e78b9c95026d572b7eaee63077
zhangshuai <1941461614@qq.com>
2019-10-28 21:31:55 +0800

    第一次修改

--- merge-eureka/src/main/java/com/bjsxt/mergeeureka/MergeEurekaApplication.java
+++ merge-eureka/src/main/java/com/bjsxt/mergeeureka/MergeEurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class MergeEurekaApplication {
 
     public static void main(String[] args) {
+        System.out.println("--");
         SpringApplication.run(MergeEurekaApplication.class, args);
     }
 
